c8b6e359b8b3b322f4aea4dd338c14f97c48b79a
JIYEON <jymerong12@naver.com>
2021-01-20 12:19:48 +0900

    첫번째 커밋

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello java world!");
+    }
+    //.java 파일을 컴파일 하면 .class 생성된다.
+    //.class파일의 종류는 바이트 코드이다.
+    //자바의 class는 JVM(자바가상머신)에서 실행된다.
+    //자바 가상머신은 지구상의 거의 모든 운영체제에 설치되어있다.
+    //JVM이 모든 OS에 설치되어 있기 때문에 자바는 플랫폼 독립성을 가질 수 있게 된다.
+    
+}
